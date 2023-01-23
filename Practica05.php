6d93f1f2f767e2eced842fad4314db1fc83da754
Evelyn Benitez <evelyn.benitez.ponce@gmail.com>
2023-01-23 11:04:35 -0600

    commit

---
+++ Practica05.php
@@ -0,0 +1,6 @@
+<?php
+#Ejemplo de declaracion  de variables tipo arr3eglo de 3
+$frutas=array("pera","manzana","uva");
+echo"Esto es una variable tipo arreglo >br>";
+echo"La fruta en la   posicion2 es:$frutas[2]";
+?>
